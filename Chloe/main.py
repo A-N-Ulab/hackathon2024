08d0392e24e897f27dd5910e9ea7111fed61b4c2
Szymon Glinka <szymon.glinka@katowice.zdz.katowice.pl>
2024-09-28 18:31:53 +0200

    new file:   Chloe/BackendPython/mainMenu.py 	modified:   Chloe/main.py 	new file:   Chloe/static/buttons.css 	modified:   Chloe/static/main.css 	new file:   Chloe/static/sidebar.css 	modified:   Chloe/templates/main.html

--- Chloe/main.py
+++ Chloe/main.py
@@ -1,34 +1,55 @@
+#=== Import libraries ===
 from flask import Flask, render_template, request, redirect, url_for
 
+#=== Create Flask app ===
 app = Flask(__name__)
 
-#website run
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    button_clicked = request.args.get('button')
-    #if button_clicked == 'Zaloguj się':
+
+#=== Logging  screen ===
+@app.route('/', methods=['GET', 'POST'])                        #define route and methods
+def logging():
+    #---- Check if the form was submitted ----
     if request.method == 'POST':
+        #
+        #---- Check which button was pressed ----
+        #---- if logIn button was pressed then check login data ----
         if request.form['button'] == 'logIn':    
-            login = request.form['login']
+            #---- Get data from form ----
+            email = request.form['email']
             password = request.form['password']
+            #
+            valid = check(email, password)                      #check login data
+            if valid == True:
+                return redirect(url_for('main'))                #redirect to main page
+        #
+        #---- if regIn button was pressed then redirect to register page ----
         elif request.form['button'] == 'regIn':
-            return redirect(url_for('register'))
+            return redirect(url_for('register'))                #redirect to register page
         
-    return render_template('login.html')
-    
-#main user interface
-@app.route('/user')
-def check(login, password):
-    print(login, password)
-    statusOfLogin = False
+    return render_template('login.html')                        #render login page
+#
+#=== Check login data ===
+def check(email, password):
+    if email == None or password == None:
+        return render_template('login.html')
+    elif email == 'admin' and password == 'admin':
+        statusOfLogin = True
+    else:
+        statusOfLogin = False
     return statusOfLogin
-    
-#register
+
+
+#=== Register ===
 @app.route('/register')
 def register():
     return render_template('rejstr.html')
 
-#password check
 
+#=== Main user interface ===
+@app.route('/user')
+def main():
+    return render_template('main.html')
+
+#=== Run app ===
 if __name__ == '__main__':
     app.run(debug=True)

4315e8904448ff3aaf595bbe3842b9a7a44146e5
Maciek <maciej.palka123@gmail.com>
2024-09-29 06:45:08 +0200

    modified:   Maciek-tests/r2.py

--- Maciek-tests/r2.py
+++ Maciek-tests/r2.py
@@ -1,7 +1,7 @@
 import re
 
 # Specify the path to your file
-file_path = 'siusiak123.txt'  # Change this to your actual file path
+file_path = 'content.txt'  # Change this to your actual file path
 
 try:
     # Open the file in read mode with UTF-8 encoding , encoding='utf-8'
